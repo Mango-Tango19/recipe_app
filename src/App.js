aeaa3a0c93247aa7c1d4cac60f427bc4eed5f0d0
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-07-05 11:35:37 +0300

    update vegetable section

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const App = () => {
             <Route path="random" element={<RandomRecipePage />} />
             <Route path="/search" element={<SearchRecipePage />} /> 
             <Route path="/chicken" element={<PopularIngridientRecipePage ingridient="chicken"/>} /> 
+            <Route path="/beef" element={<PopularIngridientRecipePage ingridient="beef"/>} /> 
+            <Route path="/vegetables" element={<PopularIngridientRecipePage ingridient="broccoli"/>} /> 
               {/* <Route path="recipes/:id" element={<SingleRecipe />} />
             */}
         </Routes>
