fd74953d2b664e79095bfaa25815ea66542a8173
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-07-18 21:04:51 +0300

    fix styles to alert

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,12 +1,14 @@
+
+
 const ErrorIndicator = ({message}) => {
   return (
     <div className="container">
     <div class="alert alert-danger" role="alert">
   <h4 class="alert-heading">Got en error!</h4>
-  <p>{ message ? message : 'Something went wrong' }</p>
+  <p>{ message ? message : 'Something went wrong' }</p> 
   <hr></hr>
-  <p class="mb-0">Please connect the support or</p>
-  <button type="button" class="btn btn-primary">Go to the main page</button>
+  <span class="mb-0">Please connect the support or </span>
+  <a href="/"  class="alert-link">go to the main page</a>
 
 </div>
     </div>
